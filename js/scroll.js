de292dbb4783c7dfd1c455499aee09ca66ef23c9
abhijit-programmer <coder.abhijit90@gmail.com>
2021-11-04 12:24:11 +0530

    Initial commit

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
